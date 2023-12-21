93ce6f627dfd72827afc4141b99a3e57ea0c60b3
ahmed yassin <83839308+79958ahmed@users.noreply.github.com>
2023-12-21 15:03:35 +0200

    app_text_style

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:good_chef_app/core/utils/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle _textStyle({
+  required Color color,
+  required double fontsize,
+  required FontWeight fontWeight,
+}){
+  return GoogleFonts.lato(
+   color: color,
+   fontSize: fontsize,
+   fontWeight: fontWeight,
+  );
+}
+
+TextStyle boldStyle({
+Color color=AppColors.white,
+double fontsize=25,
+})=>
+    _textStyle(
+        color: color,
+        fontsize: fontsize,
+        fontWeight: FontWeight.bold
+    );
+
+TextStyle regularStyle({
+  Color color=AppColors.white,
+  double fontsize=25,
+})=>
+    _textStyle(
+        color: color,
+        fontsize: fontsize,
+        fontWeight: FontWeight.normal
+    );
