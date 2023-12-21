e4cd61c8e84bd1dbc8351a221eb7129b4ee646bd
ahmed yassin <83839308+79958ahmed@users.noreply.github.com>
2023-12-21 12:27:53 +0200

    update assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,10 +1,10 @@
 class AppAssets{
   static const String basePath='assets/images/';
-  static const String Applogo='$basePath.logo.png';
-  static const String Background1='$basePath.background.png';
-  static const String Background2='$basePath.background_image2.png';
-  static const String Lock='$basePath.lock 1.png';
-  static const String ImageProfile='$basePath.profile_image.png';
+  static const String applogo='$basePath.logo.png';
+  static const String background1='$basePath.background.png';
+  static const String background2='$basePath.background_image2.png';
+  static const String lock='$basePath.lock 1.png';
+  static const String imageProfile='$basePath.profile_image.png';
 }
 
 //'{$basePath}logo.png'
