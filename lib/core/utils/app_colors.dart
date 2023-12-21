e2aa4a557c566fbdd35b03864562157ee1e125c8
ahmed yassin <83839308+79958ahmed@users.noreply.github.com>
2023-12-21 11:58:58 +0200

    app_colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static const Color primary=Color(0xffFA9A0c);
+  static const Color black =Colors.black;
+  static const Color white =Colors.white;
+  static const Color grey =Colors.grey;
+}
